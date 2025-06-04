e6b6966e4caac872fb19ed624e83d0778af24a41
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-06-04 17:26:17 +0000

    added cps test

--- js/game.js
+++ js/game.js
@@ -6,25 +6,37 @@
 
 'use strict'
 
-const timeleft = 5
-const clickCount = 0
-const timerStarted = false
-const countdownInterval = null
+let timeLeft = 5
+let clickCount = 0
+let countdownInterval = null
 
-function start () {
-  if (!timerStarted) {
-    timerStarted = true
+button.onclick = start
+
+function start() {
+  clickCount = 0
+  timeLeft = 5
+
+  document.getElementById("clicks").innerHTML = "Clicks: " + clickCount
+  document.getElementById("countdown").innerHTML = timeLeft
+  document.getElementById("answer").innerHTML = ""
+  let button = document.getElementById("click-button")
+  button.innerHTML = "Click!"
+  button.onclick = countClick
+  function countClick() {
+    clickCount++
+    document.getElementById("clicks").innerHTML = "Clicks: " + clickCount
   }
-  if (cli)
-  document.getElementById("clicks").innerHTML = clickCount
 
+  countdownInterval = setInterval(function () {
+    timeLeft--
+    document.getElementById("countdown").innerHTML = timeLeft
 
-  // Placeholder logic
-  console.log('Start button clicked')
-  console.log('Time left:', timeleft)
-  console.log('Click count:', clickCount)
-  console.log('Timer started:', timerStarted)
-  console.log('Countdown interval:', countdownInterval)
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval)
+      let cps = (clickCount / 5).toFixed(1)
+      document.getElementById("answer").innerHTML = "<p>Your CPS: " + cps + "</p>"
+        button.innerHTML = "Restart"
+        button.onclick = start
+    }
+  }, 1000)
 }
-
-start()
