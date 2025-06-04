a1285b59173aae91997c4cde05c7b55d27c09d49
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-06-04 17:41:17 +0000

    standard fix

--- js/game.js
+++ js/game.js
@@ -10,33 +10,34 @@ let timeLeft = 5
 let clickCount = 0
 let countdownInterval = null
 
+const button = document.getElementById('click-button')
 button.onclick = start
 
-function start() {
+function start () {
   clickCount = 0
   timeLeft = 5
 
-  document.getElementById("clicks").innerHTML = "Clicks: " + clickCount
-  document.getElementById("countdown").innerHTML = timeLeft
-  document.getElementById("answer").innerHTML = ""
-  let button = document.getElementById("click-button")
-  button.innerHTML = "Click!"
+  document.getElementById('clicks').innerHTML = 'Clicks: ' + clickCount
+  document.getElementById('countdown').innerHTML = timeLeft
+  document.getElementById('answer').innerHTML = ''
+  const button = document.getElementById('click-button')
+  button.innerHTML = 'Click!'
   button.onclick = countClick
-  function countClick() {
+  function countClick () {
     clickCount++
-    document.getElementById("clicks").innerHTML = "Clicks: " + clickCount
+    document.getElementById('clicks').innerHTML = 'Clicks: ' + clickCount
   }
 
   countdownInterval = setInterval(function () {
     timeLeft--
-    document.getElementById("countdown").innerHTML = timeLeft
+    document.getElementById('countdown').innerHTML = timeLeft
 
     if (timeLeft <= 0) {
       clearInterval(countdownInterval)
-      let cps = (clickCount / 5).toFixed(1)
-      document.getElementById("answer").innerHTML = "<p>Your CPS: " + cps + "</p>"
-        button.innerHTML = "Restart"
-        button.onclick = start
+      const cps = (clickCount / 5).toFixed(1)
+      document.getElementById('answer').innerHTML = '<p>Your CPS: ' + cps + '</p>'
+      button.innerHTML = 'Restart'
+      button.onclick = start
     }
   }, 1000)
 }
